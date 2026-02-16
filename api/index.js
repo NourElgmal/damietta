488ed3e3ddc30b13bd1cbcce32f580ce8263a838
NourElgmal <nouralgmal123@gmail.com>
2026-02-16 14:17:31 +0200

    fix(api): use ESM-only serverless entrypoint; remove CommonJS leftover

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@
  * Exports a default handler that forwards requests to the Express `app`.
  * Using ESM: import the app and call it as a function.
  */
-import app from '../app.js';
+import app from "../app.js";
 
 export default function handler(req, res) {
   return app(req, res);
